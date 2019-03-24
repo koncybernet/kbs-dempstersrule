872be2b295fd569abc12d4c6db2124140c7ca217
johannaSommer <johanna@mail-sommer.com>
2019-03-24 10:52:54 +0100

    minor fixes, added possibilityset structure

---
+++ PossibilitySet.py
@@ -0,0 +1,27 @@
+class PossibilitySet:
+    def __init__(self):
+        #size of list
+        self.size = 0
+        # list of entries
+        self.entrylist = []
+        # location of omega thingy
+        self.omega = 1
+
+    def add_entry(self):
+        # Add an entry for the Measure by submitting the entry-values and the probability of the entry
+
+    def get_entries(self):
+        # returns all list entries
+        return 0
+
+    def calc_belief(self):
+        # calculate belief
+        return 0
+
+    def calc_plausability(self):
+        # calculate plausability
+        return 0
+
+    def calc_doubt(self):
+        # calculate doubt
+        return 0
