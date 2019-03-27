ba079b071ddfa99a1b1197060b5097c29e8b3f53
Cathleen <ca.schmalfuss@live.de>
2019-03-27 13:33:03 +0100

    added comments about main flow

--- __main__.py
+++ __main__.py
@@ -2,3 +2,11 @@ print("Hello world!")
 f = open("dataset/emo_muster_2_1.csv", "r")
 f.readline()
 print(f.readline())
+
+# main handles the entire flow of the program
+# starts reading the file(s) -> could be outsourced later [REFACTORING]
+# handles the numbers -> probabilities
+# handles algorithm
+# handles output
+
+# "handles" means it calls the functions/ methods etc
