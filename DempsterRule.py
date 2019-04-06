b1ae3661828452bac7e58ed4569e7a3a0526e36e
johannaSommer <johanna@mail-sommer.com>
2019-04-06 16:58:11 +0200

    finished first test for dempstersrule

--- DempsterRule.py
+++ DempsterRule.py
@@ -9,28 +9,26 @@ class DempsterRule:
 
         for x in self.ev1.get_entries():
             for y in self.ev2.get_entries():
-                if x.get_values == ['omega'] and y.get_values != ['omega']:
-                    prob = x.get_probability * y.get_probability
-                    self.evout.add_entry(BasisMeasure(y.get_values, prob))
-                elif y.get_values == ['omega'] and x.get_values != ['omega']:
-                    prob = x.get_probability * y.get_probability
-                    self.evout.add_entry(BasisMeasure(x.get_values, prob))
-                elif y.get_values == ['omega'] and x.get_values == ['omega']:
-                    prob = x.get_probability * y.get_probability
+                prob = float(x.get_probability()) * float(y.get_probability())
+                if x.get_values() == ['omega'] and y.get_values() != ['omega']:
+                    self.evout.add_entry(BasisMeasure(y.get_values(), prob))
+                elif y.get_values() == ['omega'] and x.get_values() != ['omega']:
+                    self.evout.add_entry(BasisMeasure(x.get_values(), prob))
+                elif y.get_values() == ['omega'] and x.get_values() == ['omega']:
                     self.evout.add_entry(BasisMeasure(['omega'], prob))
                 else:
                     valout = []
-                    for a in y.get_values:
-                        if a in x.get_values:
+                    for a in y.get_values():
+                        if a in x.get_values():
                             valout.append(a)
                     if valout == []:
-                        # do shit
+                        self.evout.add_entry(BasisMeasure([], prob))
                     else:
-                        # unify and do other shit
-
-
+                        self.evout.add_entry(BasisMeasure(valout, prob))
 
         # add stuff together
+
+
         # refactor when smth is empty
 
     def get_output(self):
--- tests/TestDempstersRule.py
+++ tests/TestDempstersRule.py
@@ -10,7 +10,7 @@ ev2.add_entry(BasisMeasure(['sadness', 'joy'], 0.4))
 ev2.add_entry(BasisMeasure(['omega'], 0.6))
 demp = DempsterRule(ev1, ev2)
 
-expected = {BasisMeasure(['joy'], 0.28), BasisMeasure(['anger', 'joy'], 0.42), BasisMeasure(['sadness', 'joy'], 0.12), BasisMeasure(['omega'], 0.18)}
+expected = [BasisMeasure(['joy'], 0.28), BasisMeasure(['anger', 'joy'], 0.42), BasisMeasure(['sadness', 'joy'], 0.12), BasisMeasure(['omega'], 0.18)]
 
 def test_calc_demp():
-    assert (demp.get_output == expected)
+    assert demp.get_output().get_entries()[3].get_probability() == expected[3].get_probability()
