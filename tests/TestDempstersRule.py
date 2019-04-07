0cbdbef4223abd22de98c06abf9f55e262b37835
johannaSommer <johanna@mail-sommer.com>
2019-04-07 12:33:50 +0200

    added calc belief and tests

--- tests/TestDempstersRule.py
+++ tests/TestDempstersRule.py
@@ -2,6 +2,15 @@ from DempsterRule import DempsterRule
 from Evidence import Evidence
 from BasisMeasure import BasisMeasure
 
+ev5 = Evidence(None, None)
+ev5.add_entry(BasisMeasure(['anger', 'joy'], 0.5))
+ev5.add_entry(BasisMeasure(['disgust', 'joy'], 0.4))
+ev5.add_entry(BasisMeasure(['omega'], 0.1))
+ev6 = Evidence(None, None)
+ev6.add_entry(BasisMeasure(['fear', 'sorrow', 'joy'], 0.8))
+ev6.add_entry(BasisMeasure(['omega'], 0.2))
+demp3 = DempsterRule(ev6, ev5)
+
 def test_calc_demp():
     ev1 = Evidence(None, None)
     ev1.add_entry(BasisMeasure(['anger', 'joy'], 0.7))
@@ -27,26 +36,16 @@ def test_calc_empty():
     assert round(demp2.get_output().get_entries()[0].get_probability(), 5) == round(expected2[0].get_probability(), 5)
 
 def test_calc_unification():
-    ev5 = Evidence(None, None)
-    ev5.add_entry(BasisMeasure(['anger', 'joy'], 0.5))
-    ev5.add_entry(BasisMeasure(['disgust','joy'], 0.4))
-    ev5.add_entry(BasisMeasure(['omega'], 0.1))
-    ev6 = Evidence(None, None)
-    ev6.add_entry(BasisMeasure(['fear', 'sorrow', 'joy'], 0.8))
-    ev6.add_entry(BasisMeasure(['omega'], 0.2))
-    demp3 = DempsterRule(ev6, ev5)
-    expected3 = [BasisMeasure(['joy'], 0.72), BasisMeasure(['fear', 'sorrow', 'joy'], 0.08),
+    expected = [BasisMeasure(['joy'], 0.72), BasisMeasure(['fear', 'sorrow', 'joy'], 0.08),
                  BasisMeasure(['anger', 'joy'], 0.1), BasisMeasure(['disgust', 'joy'], 0.08), BasisMeasure(['omega'], 0.02)]
-    assert round(demp3.get_output().get_entries()[1].get_probability(), 5) == round(expected3[1].get_probability(), 5)
+    assert round(demp3.get_output().get_entries()[1].get_probability(), 5) == round(expected[1].get_probability(), 5)
 
 def test_calc_plausibility():
-    ev5 = Evidence(None, None)
-    ev5.add_entry(BasisMeasure(['anger', 'joy'], 0.5))
-    ev5.add_entry(BasisMeasure(['disgust', 'joy'], 0.4))
-    ev5.add_entry(BasisMeasure(['omega'], 0.1))
-    ev6 = Evidence(None, None)
-    ev6.add_entry(BasisMeasure(['fear', 'sorrow', 'joy'], 0.8))
-    ev6.add_entry(BasisMeasure(['omega'], 0.2))
-    demp3 = DempsterRule(ev6, ev5)
-    expected3 = [['joy', 0.98], ['fear', 0.08], ['sorrow', 0.08], ['anger', 0.1], ['disgust', 0.08]]
-    assert all(demp3.cal_plausibility()) == all(expected3)
+    expected = [['joy', 0.98], ['fear', 0.08], ['sorrow', 0.08], ['anger', 0.1], ['disgust', 0.08]]
+    # expected with omega:
+    # expected3 = [['joy', 1], ['fear', 0.1], ['sorrow', 0.1], ['anger', 0.12], ['disgust', 0.1]]
+    assert all(demp3.cal_plausibility()) == all(expected)
+
+def test_calc_belief():
+    expected = [['joy', 0.72], ['fear', 0], ['sorrow', 0], ['anger', 0], ['disgust', 0]]
+    assert all(demp3.cal_belief()) == all(expected)
