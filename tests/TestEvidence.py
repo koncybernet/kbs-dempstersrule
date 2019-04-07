bf4232d8068c2c91d87d37400e15161717366010
johannaSommer <johanna@mail-sommer.com>
2019-04-07 12:51:03 +0200

    comments for TestEvidence

--- tests/TestEvidence.py
+++ tests/TestEvidence.py
@@ -1,17 +1,20 @@
-from Evidence import BasisMeasure
+from Evidence import Evidence
 
 entry = 'test'
 
-
+# test if entries are appended correctly
 def test_addandgetentry():
-    bm = BasisMeasure(None, None)
+    bm = Evidence(None, None)
     bm.add_entry(entry)
     assert (bm.get_entries() == ['test'])
 
+# test if the empty constructor logic is working
 def test_emptyconstructor():
-    assert (BasisMeasure(None, None).get_entries() == [])
+    assert (Evidence(None, None).get_entries() == [])
 
-# TODO test constructing of entries with other way
-
+# test if the construction with values from table works accordingly
+def test_tableconstructor():
+    # TODO test constructing of entries with other way
+    assert 1 == 1
 
 # when done run py.test TestEvidence.py
