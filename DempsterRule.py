185c9ba8249ed304486aaaad575520eb1efd32a8
johannaSommer <johanna@mail-sommer.com>
2019-04-07 13:12:05 +0200

    comments DempsterRule

--- DempsterRule.py
+++ DempsterRule.py
@@ -1,3 +1,6 @@
+# class to implement the functionality of the Dempster Rule, accumulating two BasisMeasures into
+# one output Basis Measure
+
 from Evidence import Evidence
 from BasisMeasure import BasisMeasure
 
@@ -7,9 +10,16 @@ class DempsterRule:
         self.ev2 = ev2
         self.evout = Evidence(None, None)
 
+        # for each cell of the 'output table', create an entry in the output consisting of
+        # the values/emotions and the probability for those values/emotions. In every case,
+        # it is checked whether a BasisMeasure with exactly those values exists already. Then,
+        # no new BasisMeasure is created and instead the probability of the current object is added
+        # to the already existing one.
         for x in self.ev1.get_entries():
             for y in self.ev2.get_entries():
+                # calculate output-probability beforehand because it is needed in every case
                 prob = float(x.get_probability()) * float(y.get_probability())
+                # case 1 if one of the two inputs is omega
                 if x.get_values() == ['omega'] and y.get_values() != ['omega']:
                     exists = False
                     for c in self.evout.get_entries():
@@ -18,6 +28,7 @@ class DempsterRule:
                             exists = True
                     if not exists:
                         self.evout.add_entry(BasisMeasure(y.get_values(), prob))
+                # case 2 if one of the two inputs is omega
                 elif y.get_values() == ['omega'] and x.get_values() != ['omega']:
                     exists = False
                     for c in self.evout.get_entries():
@@ -26,14 +37,17 @@ class DempsterRule:
                             exists = True
                     if not exists:
                         self.evout.add_entry(BasisMeasure(y.get_values(), prob))
+                # case if both of the input values are omega
                 elif y.get_values() == ['omega'] and x.get_values() == ['omega']:
                     # no backlooking comparison needed since there is always just one omega output field
                     self.evout.add_entry(BasisMeasure(['omega'], prob))
+                #case if no input is omega, first check if the two inputs have emotions in common
                 else:
                     valout = []
                     for a in y.get_values():
                         if a in x.get_values():
                             valout.append(a)
+                    # if the inputs have no emotions in common, create 'empty' BasisMeasure
                     if valout == []:
                         exists = False
                         for c in self.evout.get_entries():
@@ -42,6 +56,7 @@ class DempsterRule:
                                 exists = True
                         if not exists:
                             self.evout.add_entry(BasisMeasure(['empty'], prob))
+                    # is the inputs have emotions in common, create BasisMeasure with shared emotions
                     else:
                         exists = False
                         for c in self.evout.get_entries():
@@ -51,6 +66,8 @@ class DempsterRule:
                         if not exists:
                             self.evout.add_entry(BasisMeasure(valout, prob))
 
+        # check if there is an empty element in the output. if so, determine the probability of the empty
+        # element and with the formula 1/(1-k) recalculate the remaining probabilities
         probofempty = 0
         for ind, m in enumerate(self.evout.get_entries()):
             if m.get_values() == ['empty']:
@@ -60,7 +77,7 @@ class DempsterRule:
         for n in self.evout.get_entries():
             n.set_probability(n.get_probability()*factor)
 
-        #test if sum of all probabilites is still 1
+        #test if sum of all probabilites is still 1 as a check
         sum = 0
         for o in self.evout.get_entries():
             sum += o.get_probability()
@@ -71,6 +88,7 @@ class DempsterRule:
     def get_output(self):
         return self.evout
 
+    # method to calculate the plausibility for all emotions present
     def cal_plausibility(self):
         # TODO wird Omega miteinberechnet?
         all_emotions = []
@@ -89,6 +107,7 @@ class DempsterRule:
             output.append([e, plausibility])
         return output
 
+    # method to calculate the belief for all emotions present
     def cal_belief(self):
         all_emotions = []
         output = []
